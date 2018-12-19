1adf7bedcc3ce1a7a52c366a3974fd1624379cf6
panayotZhaltov <32838824+panayotZhaltov@users.noreply.github.com>
2018-12-19 17:26:20 +0100

    Comments for the routes added

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,19 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+// Handle GET request to /orders
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Orders were fetched.'
     });
 });
 
+// Handle POST request to /orders
 router.post('/', (req, res, next) => {
     res.status(201).json({
         message: 'Orders were created.'
     });
 });
 
-
+// Handle GET request for specific IDs to /orders/orderID
 router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order details.',
@@ -22,6 +24,7 @@ router.get('/:orderId', (req, res, next) => {
     });
 });
 
+// Handle DELETE request to /orders
 router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order deleted.',
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,18 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+// Handle GET request to /products
 router.get('/', (req, res, next) => {
     res.status(201).json({
         message: 'Handling GET requests to /products'
     });
 });
 
+// Handle POST request to /products
 router.post('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handling POST requests to /products'
     });
 });
 
+// Handle GET request for specific IDs to /products/productID
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     if (id === 'special') {
@@ -28,12 +31,14 @@ router.get('/:productId', (req, res, next) => {
     }
 });
 
+// Handle PATCH request for a specific ID to /products/productID
 router.patch('/:productId', (req, res, next) => {
     res.status(200).json({
         message: 'Updated product!'
     });
 });
 
+// Handle DELETE request for a specific ID to /products/productID
 router.delete('/:productId', (req, res, next) => {
     res.status(200).json({
         message: 'Deleted Product!'
